652665db0e5ec2b71a2bc52b5dca5876d4b8fb94
Sebastian NR <snr7@live.dk>
2024-02-06 13:48:04 +0100

    Start

---
+++ AdminMenu/AdminMenu.cs
@@ -0,0 +1,22 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+
+namespace AdminMenu;
+
+public class AdminMenu : BasePlugin
+{
+    public override string ModuleName => "Admin menu";
+    public override string ModuleVersion => "0.1";
+    public override string ModuleAuthor => "WidovV";
+
+
+    [ConsoleCommand("css_adminmenu", "Open the admin menu")]
+    [ConsoleCommand("css_admin")]
+    [RequiresPermissions("css/generic")]
+    public void AdminMenuCommand(CCSPlayerController player, CommandInfo info)
+    {
+
+    }
+}
---
+++ AdminMenu/MenuConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CounterStrikeSharp.API.Core;
+
+namespace AdminMenu;
+
+internal class MenuConfig : BasePluginConfig
+{
+    public Menu[] MenuItems { get; set; } = new[]
+    {
+        new Menu
+        {
+            Title = "Player commands",
+            Commands = new[]
+            {
+                "css_kick",
+
+            }
+        }
+    };
+}
+
+public class Menu
+{
+    public string Title { get; set; }
+    public string[] Commands { get; set; }
+}
