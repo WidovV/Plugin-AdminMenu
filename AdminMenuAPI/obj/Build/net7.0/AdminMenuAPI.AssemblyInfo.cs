127f08b24c6335f741439ab39cb957d890e0169f
Sebastian NR <snr7@live.dk>
2024-02-16 12:42:12 +0100

    update

--- AdminMenuAPI/obj/Build/net7.0/AdminMenuAPI.AssemblyInfo.cs
+++ AdminMenuAPI/obj/Build/net7.0/AdminMenuAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminMenuAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Build")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b247ad2a9af073abbd03e0dd043ab420c9fbe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminMenuAPI")]
--- AdminMenuAPI/obj/Release/net7.0/AdminMenuAPI.AssemblyInfo.cs
+++ AdminMenuAPI/obj/Release/net7.0/AdminMenuAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminMenuAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb595b2981d619c17f935756efd488ca67c6fd94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b247ad2a9af073abbd03e0dd043ab420c9fbe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminMenuAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminMenuAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
