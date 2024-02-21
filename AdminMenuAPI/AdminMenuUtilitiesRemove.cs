a45d1a357416174c3c7807b8f001a4df41981494
WidovV <snr7@live.dk>
2024-02-21 20:44:29 +0100

    Add overload methods

--- AdminMenuAPI/AdminMenuUtilitiesRemove.cs
+++ AdminMenuAPI/AdminMenuUtilitiesRemove.cs
@@ -38,6 +38,36 @@ public partial class AdminMenuUtilities
         await AdminMenuHelper.UpdateConfig(menuItem, configPath, categoryPages);
         return true;
     }
+    public static async Task<bool> RemoveCategory(string modulePath, string category)
+    {
+        if (string.IsNullOrEmpty(modulePath))
+        {
+            return false;
+        }
+
+        var (menuItem, configPath) = await AdminMenuHelper.GetConfig(modulePath);
+
+        menuItem ??= new MenuConfig
+        {
+            MenuItems = new List<Menu>()
+        };
+
+        List<Menu> categoryPages = menuItem.MenuItems;
+        if (categoryPages == null)
+        {
+            return false;
+        }
+
+        if (!categoryPages.Any(x => string.Equals(x.Category, category, StringComparison.OrdinalIgnoreCase)))
+        {
+            return false;
+        }
+
+        categoryPages.RemoveAll(x => string.Equals(x.Category, category, StringComparison.OrdinalIgnoreCase));
+
+        await AdminMenuHelper.UpdateConfig(menuItem, configPath, categoryPages);
+        return true;
+    }
 
     public static async Task<bool> RemoveCommand(string modulePath, CategoryNameAttribute category, string command, string[] flags)
     {
@@ -65,6 +95,36 @@ public partial class AdminMenuUtilities
         page.Commands = page.Commands.Where(c => !(c.CommandName.Equals(command, StringComparison.OrdinalIgnoreCase) && c.Flag.SequenceEqual(flags))).ToArray();
 
         categoryPages = categoryPages.Select(x => x.Category == category.CategoryName ? page : x).ToList();
+        await AdminMenuHelper.UpdateConfig(menuItem, configPath, categoryPages);
+        return true;
+    }
+
+    public static async Task<bool> RemoveCommand(string modulePath, string category, string command, string[] flags)
+    {
+        if (string.IsNullOrEmpty(modulePath))
+        {
+            return false;
+        }
+
+        var (menuItem, configPath) = await AdminMenuHelper.GetConfig(modulePath);
+
+        menuItem ??= new MenuConfig
+        {
+            MenuItems = new List<Menu>()
+        };
+
+        List<Menu> categoryPages = menuItem.MenuItems;
+
+        var page = categoryPages.FirstOrDefault(x => string.Equals(x.Category, category, StringComparison.OrdinalIgnoreCase));
+        if (page == null)
+        {
+            return false;
+        }
+
+        // Identify the exact command to remove based on its name and flags
+        page.Commands = page.Commands.Where(c => !(c.CommandName.Equals(command, StringComparison.OrdinalIgnoreCase) && c.Flag.SequenceEqual(flags))).ToArray();
+
+        categoryPages = categoryPages.Select(x => x.Category == category ? page : x).ToList();
         await AdminMenuHelper.UpdateConfig(menuItem, configPath, categoryPages);
         return true;
     }
@@ -100,6 +160,40 @@ public partial class AdminMenuUtilities
         page.Flag = page.Flag.Where(x => !string.Equals(x, flag, StringComparison.OrdinalIgnoreCase)).ToArray();
 
         categoryPages = categoryPages.Select(x => x.Category == category ? page : x).ToList();
+        await AdminMenuHelper.UpdateConfig(menuItem, configPath, categoryPages);
+        return true;
+    }
+    public static async Task<bool> RemoveFlagFromCategory(string modulePath, string category, string flag)
+    {
+        if (string.IsNullOrEmpty(modulePath))
+        {
+            return false;
+        }
+
+        var (menuItem, configPath) = await AdminMenuHelper.GetConfig(modulePath);
+
+        menuItem ??= new MenuConfig
+        {
+            MenuItems = new List<Menu>()
+        };
+
+        List<Menu> categoryPages = menuItem.MenuItems;
+
+        if (!categoryPages.Any(x => string.Equals(x.Category, category, StringComparison.OrdinalIgnoreCase)))
+        {
+            return false;
+        }
+
+        Menu page = categoryPages.First(x => string.Equals(x.Category, category, StringComparison.OrdinalIgnoreCase));
+
+        if (!page.Flag.Contains(flag, StringComparer.OrdinalIgnoreCase))
+        {
+            return false;
+        }
+
+        page.Flag = page.Flag.Where(x => !string.Equals(x, flag, StringComparison.OrdinalIgnoreCase)).ToArray();
+        categoryPages = categoryPages.Select(x => x.Category == category ? page : x).ToList();
+
         await AdminMenuHelper.UpdateConfig(menuItem, configPath, categoryPages);
         return true;
     }
